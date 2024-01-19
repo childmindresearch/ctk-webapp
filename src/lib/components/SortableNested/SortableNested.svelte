116638feff20d80ab724809206b1e1024924049f
Reinder Vos de Wael <reinder.vosdewael@childmind.org>
2024-01-19 14:52:03 -0500

    Checkpoint

--- src/lib/components/SortableNested/SortableNested.svelte
+++ src/lib/components/SortableNested/SortableNested.svelte
@@ -1,13 +1,47 @@
 <script lang="ts">
   import type { DecisionTree } from "$lib/utils"
-  import { SlideToggle } from "@skeletonlabs/skeleton"
+  import { SlideToggle, type ModalSettings, getModalStore } from "@skeletonlabs/skeleton"
   import SortableNestedNode from "./SortableNestedNode.svelte"
+  import { shortenText } from "./utils"
+  import { patchDiagnosis } from "$lib/api"
 
   export let node: DecisionTree
   export let editable = true
+
+  const modalStore = getModalStore()
+
+  async function onDrag(event: CustomEvent) {
+    const targetId = parseInt(event.detail.to.id.split("-")[1])
+    const sourceId = parseInt(event.detail.from.id.split("-")[1])
+
+    const sourceNode = node.getNodeById(sourceId)
+    const targetParentNode = node.getNodeById(targetId)?.parent
+
+    if (!sourceNode || !targetParentNode) return
+    const modal: ModalSettings = {
+      type: "confirm",
+      title: "Move diagnosis",
+      body: `Are you sure you want to move "${shortenText(sourceNode.text)}" to "${shortenText(
+        targetParentNode.text
+      )}"?`,
+      response: async confirmed => {
+        if (confirmed) {
+          await patchDiagnosis(sourceId, undefined, targetId)
+        } else {
+          const items = event.detail.from.querySelectorAll(":scope > div")
+          event.detail.from.insertBefore(
+            event.detail.item,
+            items[event.detail.oldIndex + (event.detail.oldIndex > event.detail.newIndex)]
+          )
+          return false
+        }
+      }
+    }
+    modalStore.trigger(modal)
+  }
 </script>
 
 <div>
   <SlideToggle name="slider-editable" size="sm" bind:checked={editable}>Editable</SlideToggle>
 </div>
-<SortableNestedNode {node} bind:editable isRoot={true} on:save />
+<SortableNestedNode {node} bind:editable isRoot={true} on:save on:drag={onDrag} />
--- src/lib/components/SortableNested/SortableNestedNode.svelte
+++ src/lib/components/SortableNested/SortableNestedNode.svelte
@@ -6,8 +6,6 @@
   import { slide } from "svelte/transition"
   import AdminButtons from "./AdminButtons.svelte"
   import { shortenText } from "./utils"
-  import { getModalStore, type ModalSettings } from "@skeletonlabs/skeleton"
-  import { patchDiagnosis } from "$lib/api"
 
   export let node: DecisionTree
   export let editable = false
@@ -17,55 +15,6 @@
   let sorter: Sortable
 
   const dispatch = createEventDispatcher()
-  const modalStore = getModalStore()
-
-  async function OnEnd(event: SortableEvent) {
-    const customEvent = new CustomEvent("end", {
-      detail: {
-        oldIndex: event.oldIndex,
-        newIndex: event.newIndex,
-        to: event.to,
-        from: event.from,
-        item: event.item
-      }
-    })
-    onDrag(customEvent)
-  }
-
-  async function onDrag(event: CustomEvent) {
-    if (!isRoot) {
-      dispatch("drag", event.detail)
-      return
-    }
-
-    const targetId = parseInt(event.detail.to.id.split("-")[1])
-    const sourceId = parseInt(event.detail.from.id.split("-")[1])
-
-    const sourceNode = node.getNodeById(sourceId)
-    const targetParentNode = node.getNodeById(targetId)?.parent
-
-    if (!sourceNode || !targetParentNode) return
-    const modal: ModalSettings = {
-      type: "confirm",
-      title: "Move diagnosis",
-      body: `Are you sure you want to move "${shortenText(sourceNode.text)}" to "${shortenText(
-        targetParentNode.text
-      )}"?`,
-      response: async confirmed => {
-        if (confirmed) {
-          await patchDiagnosis(sourceId, undefined, targetId)
-        } else {
-          const items = event.detail.from.querySelectorAll(":scope > div")
-          event.detail.from.insertBefore(
-            event.detail.item,
-            items[event.detail.oldIndex + (event.detail.oldIndex > event.detail.newIndex)]
-          )
-          return false
-        }
-      }
-    }
-    modalStore.trigger(modal)
-  }
 
   function fold() {
     if (isRoot) return
@@ -84,7 +33,15 @@
       },
       disabled: true,
       animation: 100,
-      onEnd: OnEnd
+      onEnd: (event: SortableEvent) => {
+        dispatch("drag", {
+          oldIndex: event.oldIndex,
+          newIndex: event.newIndex,
+          to: event.to,
+          from: event.from,
+          item: event.item
+        })
+      }
     })
   })
 
@@ -112,7 +69,7 @@
     {#if !isFolded}
       <div class="border-secondary-500 border-l-2 pl-3 mb-3" transition:slide>
         {#each node.children as child}
-          <svelte:self bind:node={child} bind:editable isRoot={false} on:drag={onDrag} on:save on:create />
+          <svelte:self bind:node={child} bind:editable isRoot={false} on:drag on:save on:create />
         {/each}
       </div>
     {/if}
