5343f92173055a60268fd3898a7d8ecbae95c2ba
Reinder Vos de Wael <reinder.vosdewael@childmind.org>
2024-03-27 13:49:04 -0400

    Refactor load function to return diagnoses as an object

--- src/routes/diagnoses/+page.server.ts
+++ src/routes/diagnoses/+page.server.ts
@@ -3,5 +3,5 @@ import type { SqlDiagnosisSchema } from "$lib/server/sql"
 
 export async function load() {
   const database = getDatabase()
-  return (await database`SELECT * FROM diagnoses`) as SqlDiagnosisSchema[]
+  return { diagnoses: (await database`SELECT * FROM diagnoses`) as SqlDiagnosisSchema[] }
 }
