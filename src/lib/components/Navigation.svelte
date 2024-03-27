72eee9d793efcead2c103094efab4a73f98729a4
Reinder Vos de Wael <reinder.vosdewael@childmind.org>
2024-03-27 11:53:29 -0400

    Add sidebar for navigation

---
+++ src/lib/components/Navigation.svelte
@@ -0,0 +1,27 @@
+<script lang="ts">
+  import { page } from "$app/stores"
+  import { getDrawerStore } from "@skeletonlabs/skeleton"
+
+  const drawerStore = getDrawerStore()
+  const pages = [
+    { name: "Diagnoses", href: "/diagnoses" },
+    { name: "Intake", href: "/intake" },
+    { name: "Summarization", href: "/summarization" }
+  ]
+
+  function drawerClose(): void {
+    drawerStore.close()
+  }
+
+  $: classesActive = (href: string) => (href === $page.url.pathname ? "bg-primary-100 dark:bg-primary-700" : "")
+</script>
+
+<nav class="list-nav p-4 flex flex-col" data-testid="div-navigation">
+  <ul style="flex-grow: 1">
+    {#each pages as { name, href }}
+      <li>
+        <a {href} class={classesActive(href)} data-testid="a-navigation" on:click={drawerClose}>{name}</a>
+      </li>
+    {/each}
+  </ul>
+</nav>
--- src/routes/+page.svelte
+++
@@ -1,7 +0,0 @@
-<script lang="ts">
-  import { onMount } from "svelte"
-
-  onMount(() => {
-    window.location.href = "/diagnoses"
-  })
-</script>
