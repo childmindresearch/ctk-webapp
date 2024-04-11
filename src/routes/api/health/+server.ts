e96e04cd023f5be483b931aa07702080981eb0d7
Reinder Vos de Wael <reinder.vosdewael@childmind.org>
2024-04-11 11:11:13 -0400

    Call the health endpoint to keep the function warm and avoid cold starts

---
+++ src/routes/api/health/+server.ts
@@ -0,0 +1,20 @@
+import { env } from "$env/dynamic/private"
+import { logger } from "$lib/server/logging"
+
+export async function GET() {
+    logger.info("Warming up the server.")
+    const headers = new Headers({ "x-functions-key": env.AZURE_FUNCTION_PYTHON_KEY || "" })
+    return await fetch(`${env.AZURE_FUNCTION_PYTHON_URL}/health`, { headers: headers })
+        .then(async response => {
+            if (response.ok && response.body) {
+                return new Response(response.body)
+            } else {
+                throw new Error(await response.text())
+            }
+        })
+        .catch(error => {
+            console.log(error)
+            logger.error(`Error warming up the server: ${error}`)
+            return new Response(error, { status: 500 })
+        })
+}
