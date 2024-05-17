b9b5d6712252e8ebf88d655db32914897dbade2d
Reinder Vos de Wael <reinder.vosdewael@childmind.org>
2024-05-17 13:10:19 -0400

    Add experimental wopi api

---
+++ src/routes/experimental/diagnoses/+page.svelte
@@ -0,0 +1,11 @@
+<script lang="ts">
+    const wopisrc: string = "http://localhost:3000/api/experimental/wopi/files/test3.docx?access_token=fake"
+    const encodedWopisrc: string = encodeURIComponent(wopisrc)
+</script>
+
+<iframe
+    title={"Editor"}
+    src={`http://localhost:9980/browser/80a6f97/cool.html?WOPISrc=${encodedWopisrc}`}
+    width="90%"
+    height="1200px"
+></iframe>
