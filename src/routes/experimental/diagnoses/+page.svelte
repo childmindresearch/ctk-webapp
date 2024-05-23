f7bcea79222704087429449dc32d404171830fd2
Reinder Vos de Wael <reinder.vosdewael@childmind.org>
2024-05-23 16:29:33 -0400

    Update WOPI src

--- src/routes/experimental/diagnoses/+page.svelte
+++ src/routes/experimental/diagnoses/+page.svelte
@@ -1,6 +1,6 @@
 <script lang="ts">
     const wopisrc: string =
-        "https://ctk-functions-python.jollybeach-29614a80.eastus.azurecontainerapps.io/api/experimental/wopi/files/test3.docx?access_token=fake"
+        "https://cliniciantoolkit.azurewebsites.net/api/experimental/wopi/files/test3.docx?access_token=fake"
     const encodedWopisrc: string = encodeURIComponent(wopisrc)
 </script>
 
