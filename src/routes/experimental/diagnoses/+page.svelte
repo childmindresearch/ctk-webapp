a38b9733f8dcc7500dcf645837f7d8d8ff92e653
Reinder Vos de Wael <reinder.vosdewael@childmind.org>
2024-05-23 16:28:26 -0400

    Experimental online WOPI

--- src/routes/experimental/diagnoses/+page.svelte
+++ src/routes/experimental/diagnoses/+page.svelte
@@ -1,11 +1,12 @@
 <script lang="ts">
-    const wopisrc: string = "http://localhost:3000/api/experimental/wopi/files/test3.docx?access_token=fake"
+    const wopisrc: string =
+        "https://ctk-functions-python.jollybeach-29614a80.eastus.azurecontainerapps.io/api/experimental/wopi/files/test3.docx?access_token=fake"
     const encodedWopisrc: string = encodeURIComponent(wopisrc)
 </script>
 
 <iframe
     title={"Editor"}
-    src={`http://localhost:9980/browser/80a6f97/cool.html?WOPISrc=${encodedWopisrc}`}
+    src={`https://libreoffice.jollybeach-29614a80.eastus.azurecontainerapps.io/browser/80a6f97/cool.html?WOPISrc=${encodedWopisrc}`}
     width="90%"
     height="1200px"
 ></iframe>
