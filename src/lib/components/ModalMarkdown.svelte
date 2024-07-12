3a7377d4246655487aad104956f5a0731e8549e4
Reinder Vos de Wael <reinder.vosdewael@childmind.org>
2024-07-12 15:10:32 -0400

    feat: add template value detection

--- src/lib/components/ModalMarkdown.svelte
+++ src/lib/components/ModalMarkdown.svelte
@@ -4,6 +4,8 @@
 
     const modalStore = getModalStore()
     let text = $modalStore[0].meta.value
+    let replacementTags: string[] = []
+    let warningTags: string[] = []
 
     function onSubmit(event: Event) {
         event.preventDefault()
@@ -12,6 +14,34 @@
         }
         modalStore.close()
     }
+
+    function detectTags(text: string, regex: RegExp) {
+        const match = text.match(regex)
+        if (!match) return []
+
+        function matchToText(tag: string) {
+            const text = tag.match(/[0-9a-zA-Z-]+/g)
+            if (!text) return ""
+            const addSpace = text[0].replace(/-/g, " ")
+            const capitalize = addSpace.toLowerCase().replace(/\b\w/g, (char: string) => char.toUpperCase())
+            return capitalize
+        }
+
+        const tags = match.map(tag => matchToText(tag))
+        const uniqueTags = [...new Set(tags)]
+        const pronounMapping: { [key: string]: string } = {
+            "Pronoun 0": "Pronoun 0 (he/she/they)",
+            "Pronoun 1": "Pronoun 1 (him/her/them)",
+            "Pronoun 2": "Pronoun 2 (his/her/their)",
+            "Pronoun 3": "Pronoun 3 (his/hers/theirs)",
+            "Pronoun 4": "Pronoun 4 (himself/herself/themself)"
+        }
+
+        return uniqueTags.map(tag => pronounMapping[tag] || tag)
+    }
+
+    $: replacementTags = detectTags(text, /{{\s*[\w-]+\s*}}/g)
+    $: warningTags = detectTags(text, /{{!\s*[\w-]+\s*}}/g)
 </script>
 
 {#if $modalStore[0]}
@@ -22,9 +52,40 @@
         {#if $modalStore[0].meta.instructions}
             <p>{$modalStore[0].meta.instructions}</p>
         {/if}
-        <article>
-            <MarkdownEditor value={$modalStore[0].meta.value} on:input={event => (text = event.detail)} />
-        </article>
+
+        <div>
+            <div class="grid grid-cols-2 pb-2">
+                <div>
+                    <h2 class="text-lg">Replacement tags</h2>
+                    <ol class="list">
+                        {#if replacementTags.length > 0}
+                            {#each replacementTags as tag}
+                                <li>• {tag}</li>
+                            {/each}
+                        {:else}
+                            <li>• No replacement tags found</li>
+                        {/if}
+                    </ol>
+                </div>
+
+                <div>
+                    <h2 class="text-lg">Warning tags</h2>
+                    <ul>
+                        {#if warningTags.length > 0}
+                            {#each warningTags as tag}
+                                <li>• {tag}</li>
+                            {/each}
+                        {:else}
+                            <li>• No warning tags found</li>
+                        {/if}
+                    </ul>
+                </div>
+            </div>
+            <div>
+                <MarkdownEditor value={$modalStore[0].meta.value} on:input={event => (text = event.detail)} />
+            </div>
+        </div>
+
         <button class="btn variant-filled-primary" on:click={onSubmit}>Save</button>
     </div>
 {/if}
