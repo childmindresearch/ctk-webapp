383ebfb309ce2145cdc8d2e221bf5aa409855b6e
Reinder Vos de Wael <reinder.vosdewael@childmind.org>
2024-07-19 15:14:45 -0400

    fix: markdown lists now properly indent

--- src/lib/components/Navigation.svelte
+++ src/lib/components/Navigation.svelte
@@ -1,27 +1,27 @@
 <script lang="ts">
-  import { page } from "$app/stores"
-  import { getDrawerStore } from "@skeletonlabs/skeleton"
+    import { page } from "$app/stores"
+    import { getDrawerStore } from "@skeletonlabs/skeleton"
 
-  const drawerStore = getDrawerStore()
-  const pages = [
-    { name: "Intake", href: "/intake" },
-    { name: "Summarization", href: "/summarization" },
-    { name: "Templates", href: "/templates" },
-  ]
+    const drawerStore = getDrawerStore()
+    const pages = [
+        { name: "Intake", href: "/intake" },
+        { name: "Summarization", href: "/summarization" },
+        { name: "Templates", href: "/templates" }
+    ]
 
-  function drawerClose(): void {
-    drawerStore.close()
-  }
+    function drawerClose(): void {
+        drawerStore.close()
+    }
 
-  $: classesActive = (href: string) => (href === $page.url.pathname ? "bg-primary-100 dark:bg-primary-700" : "")
+    $: classesActive = (href: string) => (href === $page.url.pathname ? "bg-primary-100 dark:bg-primary-700" : "")
 </script>
 
 <nav class="list-nav p-4 flex flex-col" data-testid="div-navigation">
-  <ul style="flex-grow: 1">
-    {#each pages as { name, href }}
-      <li>
-        <a {href} class={classesActive(href)} data-testid="a-navigation" on:click={drawerClose}>{name}</a>
-      </li>
-    {/each}
-  </ul>
+    <ul class="list-none" style="flex-grow: 1">
+        {#each pages as { name, href }}
+            <li>
+                <a {href} class={classesActive(href)} data-testid="a-navigation" on:click={drawerClose}>{name}</a>
+            </li>
+        {/each}
+    </ul>
 </nav>
