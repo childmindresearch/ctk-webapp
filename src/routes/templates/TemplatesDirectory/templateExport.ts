8cc1d8f3dc91e9c497a92baa863b4ec014f377d9
Reinder Vos de Wael <reinder.vosdewael@childmind.org>
2024-07-25 15:20:25 -0400

    feat: add tab support

--- src/routes/templates/TemplatesDirectory/templateExport.ts
+++ src/routes/templates/TemplatesDirectory/templateExport.ts
@@ -8,5 +8,8 @@ export function nodesToMarkdown(node: DecisionTree, depth: number = 0): string {
         header = "#".repeat(depth + 1) + " "
     }
     const text = giveMarkdownUrlsHyperlinks(node.text)
-    return header + `${text}\n` + node.children.map(child => nodesToMarkdown(child, depth + 1)).join("\n")
+    const full_text = header + `${text}\n` + node.children.map(child => nodesToMarkdown(child, depth + 1)).join("\n")
+
+    // Pandoc does not support tabs. We use |t as a workaround.
+    return full_text.replace(/\t/g, "|t")
 }
