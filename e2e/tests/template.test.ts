c0a57c17e7c2fa2f7583e2ebc4c98443d529cf4c
reindervdw-cmi <reinder.vosdewael@childmind.org>
2025-11-07 13:47:56 -0500

    Fix/e2e template test (#242)
    * attempt fix template e2e failure
    
    * attempt fixe2e failure

--- e2e/tests/template.test.ts
+++ e2e/tests/template.test.ts
@@ -5,16 +5,17 @@ test("the user can navigate to the templates page and download a template .docx 
     await page.locator(".lucide-folder-closed").nth(0).waitFor()
 
     const folder = page.locator(".lucide-folder-closed").nth(0)
-    await folder.click()
+    await folder.waitFor({ state: "visible" })
+    await folder.click({ force: true })
 
     const leaf = page.locator(".lucide-file-plus-2").nth(0)
+    await leaf.waitFor({ state: "visible" })
     await leaf.click()
 
     const generationPage = page.getByRole("tab").nth(2)
     await generationPage.click()
 
     const downloadButton = page.getByText("Download").nth(1)
-    console.log(downloadButton)
     await downloadButton.click()
 
     const downloadPromise = page.waitForEvent("download")
