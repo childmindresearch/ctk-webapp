7688631444fff1f1091499c8b804061fe22e3458
reindervdw-cmi <reinder.vosdewael@childmind.org>
2025-11-07 13:25:40 -0500

    fix failing template e2e test (#241)

--- e2e/tests/template.test.ts
+++ e2e/tests/template.test.ts
@@ -1,7 +1,6 @@
 import { test, expect } from "@playwright/test"
 
 test("the user can navigate to the templates page and download a template .docx file", async ({ page }) => {
-    test.slow()
     await page.goto("/templates")
     await page.locator(".lucide-folder-closed").nth(0).waitFor()
 
@@ -11,10 +10,11 @@ test("the user can navigate to the templates page and download a template .docx 
     const leaf = page.locator(".lucide-file-plus-2").nth(0)
     await leaf.click()
 
-    const generationPage = page.getByText("Report Generation")
+    const generationPage = page.getByRole("tab").nth(2)
     await generationPage.click()
 
-    const downloadButton = page.getByText("Download")
+    const downloadButton = page.getByText("Download").nth(1)
+    console.log(downloadButton)
     await downloadButton.click()
 
     const downloadPromise = page.waitForEvent("download")
