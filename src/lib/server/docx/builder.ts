d6c751c6bfba72209fc3d986436fefef3aa89e23
reindervdw-cmi <reinder.vosdewael@childmind.org>
2025-11-20 09:34:18 -0500

    fix eslint

--- src/lib/server/docx/builder.ts
+++ src/lib/server/docx/builder.ts
@@ -1,6 +1,5 @@
 import { DocxBuilderClient, resolveProps, type AwaitableProps } from "$lib/docx/builder"
 import { type PatchDocumentOptions, patchDocument } from "docx"
-import AdmZip from "adm-zip"
 
 export class DocxBuilderServer extends DocxBuilderClient {
     public async patchDocument(
