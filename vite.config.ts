e7d561c68b67ade92b1539c2111abf38182958a6
reindervdw-cmi <reinder.vosdewael@childmind.org>
2025-11-28 10:55:18 -0500

    feat: Report version on website startup

--- vite.config.ts
+++ vite.config.ts
@@ -2,12 +2,16 @@ import { sveltekit } from "@sveltejs/kit/vite"
 import { defineConfig } from "vitest/config"
 import tailwindcss from "@tailwindcss/vite"
 import devtoolsJson from "vite-plugin-devtools-json"
+import pkg from "./package.json"
 
 export default defineConfig(({ mode }) => ({
     plugins: [sveltekit(), tailwindcss(), devtoolsJson()],
     assetsInclude: ["**/*.docx"],
     resolve: {
         conditions: mode === "test" ? ["browser"] : []
+    },
+    define: {
+        "import.meta.env.VITE_APP_VERSION": JSON.stringify(pkg.version)
     },
     test: {
         globals: true,
