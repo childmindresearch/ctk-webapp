011db4fcf17a32c6c7b8ecb7ee7b58af57d5cff2
reindervdw-cmi <reinder.vosdewael@childmind.org>
2025-12-30 16:24:13 -0500

    feat: Add Azure export of OpenTelemetry (#276)

--- src/instrumentation.server.ts
+++ src/instrumentation.server.ts
@@ -1,15 +1,27 @@
 import { NodeSDK } from "@opentelemetry/sdk-node"
+import { BatchSpanProcessor } from "@opentelemetry/sdk-trace-base"
 import { getNodeAutoInstrumentations } from "@opentelemetry/auto-instrumentations-node"
 import { OTLPTraceExporter } from "@opentelemetry/exporter-trace-otlp-proto"
 import { createAddHookMessageChannel } from "import-in-the-middle"
 import { register } from "node:module"
+import { AzureMonitorTraceExporter } from "@azure/monitor-opentelemetry-exporter"
+import { env } from "$env/dynamic/private"
+import { dev } from "$app/environment"
 
 const { registerOptions } = createAddHookMessageChannel()
 register("import-in-the-middle/hook.mjs", import.meta.url, registerOptions)
 
+const spanProcessors = [new BatchSpanProcessor(new OTLPTraceExporter())]
+if (!dev) {
+    spanProcessors.push(
+        new BatchSpanProcessor(
+            new AzureMonitorTraceExporter({ connectionString: env.APPLICATIONINSIGHTS_CONNECTION_STRING })
+        )
+    )
+}
 const sdk = new NodeSDK({
     serviceName: "ctk-tracing",
-    traceExporter: new OTLPTraceExporter(),
+    spanProcessors: spanProcessors,
     instrumentations: [getNodeAutoInstrumentations()]
 })
 
